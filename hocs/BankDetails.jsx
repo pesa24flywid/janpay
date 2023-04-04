c512441cbca42b2a0098bd2db658cd38d82a9671
DezyNation <dezynationindia@gmail.com>
2023-04-05 02:41:02 +0530

    settlement done

--- hocs/BankDetails.jsx
+++ hocs/BankDetails.jsx
@@ -8,8 +8,8 @@ import { ClientAxios } from '../lib/axios'
 
 const BankDetails = () => {
     const [bankDetails, setBankDetails] = useState([])
-    useEffect(()=>{
-        ClientAxios.post('/api/cms/banks/fetch').then((res)=>{
+    useEffect(() => {
+        ClientAxios.post('/api/cms/banks/fetch').then((res) => {
             setBankDetails(res.data)
         })
     }, [])
@@ -25,17 +25,21 @@ const BankDetails = () => {
                     direction={'column'}
                     gap={2}
                 >
-                    {bankDetails.map((detail, key) => (
-                        <Box
-                            key={key}
-                            border={'1px'} rounded={'inherit'}
-                            borderColor={'gray.200'} p={2}
-                        >
-                            <Text fontSize={'12'} mb={2}><b>Bank: </b> {detail.bank_name}</Text>
-                            <Text fontSize={'12'} mb={2}><b>Account: </b> {detail.account}</Text>
-                            <Text fontSize={'12'} mb={2}><b>IFSC: </b> {detail.ifsc}</Text>
-                        </Box>
-                    ))}
+                    {bankDetails.map((detail, key) => {
+                        if (detail.status) {
+                            return (
+                                <Box
+                                    key={key}
+                                    border={'1px'} rounded={'inherit'}
+                                    borderColor={'gray.200'} p={2}
+                                >
+                                    <Text fontSize={'12'} mb={2}><b>Bank: </b> {detail.bank_name}</Text>
+                                    <Text fontSize={'12'} mb={2}><b>Account: </b> {detail.account}</Text>
+                                    <Text fontSize={'12'} mb={2}><b>IFSC: </b> {detail.ifsc}</Text>
+                                </Box>
+                            )
+                        }
+                    })}
                 </Stack>
             </Box>
         </>
