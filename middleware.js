c539308ffb834358084a07e84f188b0a65c48304
Dezynation <dezynationindia@gmail.com>
2023-04-05 17:11:41 +0530

    middleware fixed

--- middleware.js
+++ middleware.js
@@ -6,11 +6,11 @@ const Middleware = (req) => {
 
     const url = req.url
     
-    if(!verified && url.includes("/dashboard")){
+    if(!verified && url.includes("in/dashboard")){
         return NextResponse.redirect(process.env.NEXT_PUBLIC_FRONTEND_URL+"/auth/login")
     }
     else if(verified && url.includes("/auth")){
-        return NextResponse.redirect(process.env.NEXT_PUBLIC_FRONTEND_URL+"/dashboard")
+        return NextResponse.redirect(process.env.NEXT_PUBLIC_FRONTEND_URL+"in/dashboard")
     }
 }
 
