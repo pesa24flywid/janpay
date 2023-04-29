dda04230b0071eb4054f7a18ee5a8ce501ce3a82
DezyNation <dezynationindia@gmail.com>
2023-04-29 19:50:20 +0530

    menifest linked

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/ios/144.png" />
+        <meta name='theme-color' content='#CE5959' />
+      </Head>
       <body>
         <Main />
         <NextScript />
