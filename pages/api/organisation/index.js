54596d308746afce30b87bcb0ac3381e4a0cb33f
Dezynation <dezynationindia@gmail.com>
2023-05-25 19:42:22 +0530

    organisation model updated

---
+++ pages/api/organisation/index.js
@@ -0,0 +1,17 @@
+import Organisation from "@/lib/models/OrganisationModel";
+import Connect from "@/lib/utils/mongoose";
+
+export default async function GlobalDetails(req, res) {
+    await Connect()
+    if (req.method == "GET") {
+        const result = await Organisation.findOne({ organisation_code: process.env.NEXT_PUBLIC_ORGANISATION })
+        if (!result) return res.status(404).json({ message: "Organisation Info not found!" })
+        return res.status(200).json(result)
+    }
+
+    if (req.method == "POST") {
+        const result = await Organisation.findOneAndUpdate({organisation_code: process.env.NEXT_PUBLIC_ORGANISATION}, req.body).exec()
+        if (!result) return res.status(500).json({ message: "Error while updating" })
+        return res.status(200).json(result)
+    }
+}
