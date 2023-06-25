e563024e29d0db9ef2b2eb25cab15618453b1a9e
DezyNation <dezynationindia@gmail.com>
2023-06-26 01:42:09 +0530

    creds otp

--- pages/dashboard/profile/reset-mpin.jsx
+++ pages/dashboard/profile/reset-mpin.jsx
@@ -1,110 +1,193 @@
-import React from 'react'
+import React from "react";
 import {
-    HStack,
-    Box,
-    Stack,
-    VStack,
-    Text,
-    FormControl,
-    FormLabel,
-    PinInput,
-    PinInputField,
-    Button,
-    useToast
-} from '@chakra-ui/react'
+  HStack,
+  Box,
+  Stack,
+  VStack,
+  Text,
+  FormControl,
+  FormLabel,
+  PinInput,
+  PinInputField,
+  Button,
+  useToast,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { useFormik } from "formik";
-import DashboardWrapper from '../../../hocs/DashboardLayout';
-import BackendAxios from '../../../lib/axios';
+import DashboardWrapper from "../../../hocs/DashboardLayout";
+import BackendAxios from "../../../lib/axios";
+import { Modal } from "@chakra-ui/react";
+import { ModalOverlay } from "@chakra-ui/react";
+import { ModalContent } from "@chakra-ui/react";
+import { ModalHeader } from "@chakra-ui/react";
+import { ModalBody } from "@chakra-ui/react";
+import { ModalFooter } from "@chakra-ui/react";
 
 const ResetMpin = () => {
-    const Toast = useToast({
-        position: 'top-right'
-    })
+  const Toast = useToast({
+    position: "top-right",
+  });
+  const { isOpen, onToggle } = useDisclosure();
 
-    const MpinFormik = useFormik({
-        initialValues: {
-            old_mpin: "",
-            new_mpin: "",
-            new_mpin_confirmation: "",
-        }
-    })
+  const MpinFormik = useFormik({
+    initialValues: {
+      old_mpin: "",
+      new_mpin: "",
+      new_mpin_confirmation: "",
+      otp:""
+    },
+  });
 
-    function handleMpinReset() {
-        BackendAxios.post('/api/user/new-mpin', JSON.stringify({
-            old_mpin: MpinFormik.values.old_mpin,
-            new_mpin: MpinFormik.values.new_mpin,
-            new_mpin_confirmation: MpinFormik.values.new_mpin_confirmation,
-        })).then((res) => {
-            Toast({
-                status: 'success',
-                title: 'Success',
-                description: 'Your MPIN was changed succesfully.'
-            })
-        }).catch((err) => {
-            Toast({
-                status: 'error',
-                title: 'Error Occured',
-                description: err.response.data.message || err.response.data || err.message,
-            })
-        })
-    }
-
-    return (
-        <>
-            <DashboardWrapper titleText={'Reset MPIN'}>
-                <HStack w={'full'} justifyContent={'center'}>
-                    <Box
-                        bg={'white'}
-                        boxShadow={'md'}
-                        p={6} w={['full', 'md']}
-                        rounded={16}
-                        display={'flex'}
-                        flexDirection={'column'}
-                        justifyContent={'center'}
-                        alignItems={'center'}
-                    >
-                        <Text fontSize={'lg'} mb={12}>Reset Your MPIN</Text>
-                        <VStack spacing={8}>
-                            <FormControl>
-                                <FormLabel textAlign={'center'}>Enter Old MPIN</FormLabel>
-                                <HStack spacing={6} justifyContent={'center'}>
-                                    <PinInput otp onChange={(value) => MpinFormik.setFieldValue('old_mpin', value)}>
-                                        <PinInputField bg={'aqua'} />
-                                        <PinInputField bg={'aqua'} />
-                                        <PinInputField bg={'aqua'} />
-                                        <PinInputField bg={'aqua'} />
-                                    </PinInput>
-                                </HStack>
-                            </FormControl>
-                            <FormControl >
-                                <FormLabel textAlign={'center'}>Enter New MPIN</FormLabel>
-                                <HStack spacing={6} justifyContent={'center'}>
-                                    <PinInput otp mask onChange={(value) => MpinFormik.setFieldValue('new_mpin', value)}>
-                                        <PinInputField bg={'aqua'} />
-                                        <PinInputField bg={'aqua'} />
-                                        <PinInputField bg={'aqua'} />
-                                        <PinInputField bg={'aqua'} />
-                                    </PinInput>
-                                </HStack>
-                            </FormControl>
-                            <FormControl >
-                                <FormLabel textAlign={'center'}>Verify New MPIN</FormLabel>
-                                <HStack spacing={6} justifyContent={'center'}>
-                                    <PinInput otp onChange={(value) => MpinFormik.setFieldValue('new_mpin_confirmation', value)}>
-                                        <PinInputField bg={'aqua'} />
-                                        <PinInputField bg={'aqua'} />
-                                        <PinInputField bg={'aqua'} />
-                                        <PinInputField bg={'aqua'} />
-                                    </PinInput>
-                                </HStack>
-                            </FormControl>
-                            <Button colorScheme={'orange'} onClick={handleMpinReset}>Done</Button>
-                        </VStack>
-                    </Box>
-                </HStack>
-            </DashboardWrapper>
-        </>
+  function handleMpinReset() {
+    BackendAxios.post(
+      "/api/user/new-mpin",
+      JSON.stringify({
+        old_mpin: MpinFormik.values.old_mpin,
+        new_mpin: MpinFormik.values.new_mpin,
+        new_mpin_confirmation: MpinFormik.values.new_mpin_confirmation,
+        otp:MpinFormik.values.otp
+      })
     )
-}
+      .then((res) => {
+        Toast({
+          status: "success",
+          title: "Success",
+          description: "Your MPIN was changed succesfully.",
+        });
+      })
+      .catch((err) => {
+        Toast({
+          status: "error",
+          title: "Error Occured",
+          description:
+            err.response.data.message || err.response.data || err.message,
+        });
+      });
+  }
 
-export default ResetMpin
+  function sendOtp() {
+    BackendAxios.post("/password/send-otp")
+      .then((res) => {
+        onToggle();
+        Toast({
+          status: "success",
+          description: "OTP Sent To Your Phone",
+        });
+      })
+      .catch((err) => {
+        Toast({
+          status: "error",
+          description:
+            err?.response?.data?.message || err?.response?.data || err?.message,
+        });
+      });
+  }
+
+  return (
+    <>
+      <DashboardWrapper titleText={"Reset MPIN"}>
+        <HStack w={"full"} justifyContent={"center"}>
+          <Box
+            bg={"white"}
+            boxShadow={"md"}
+            p={6}
+            w={["full", "md"]}
+            rounded={16}
+            display={"flex"}
+            flexDirection={"column"}
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            <Text fontSize={"lg"} mb={12}>
+              Reset Your MPIN
+            </Text>
+            <VStack spacing={8}>
+              <FormControl>
+                <FormLabel textAlign={"center"}>Enter Old MPIN</FormLabel>
+                <HStack spacing={6} justifyContent={"center"}>
+                  <PinInput
+                    otp
+                    onChange={(value) =>
+                      MpinFormik.setFieldValue("old_mpin", value)
+                    }
+                  >
+                    <PinInputField bg={"aqua"} />
+                    <PinInputField bg={"aqua"} />
+                    <PinInputField bg={"aqua"} />
+                    <PinInputField bg={"aqua"} />
+                  </PinInput>
+                </HStack>
+              </FormControl>
+              <FormControl>
+                <FormLabel textAlign={"center"}>Enter New MPIN</FormLabel>
+                <HStack spacing={6} justifyContent={"center"}>
+                  <PinInput
+                    otp
+                    mask
+                    onChange={(value) =>
+                      MpinFormik.setFieldValue("new_mpin", value)
+                    }
+                  >
+                    <PinInputField bg={"aqua"} />
+                    <PinInputField bg={"aqua"} />
+                    <PinInputField bg={"aqua"} />
+                    <PinInputField bg={"aqua"} />
+                  </PinInput>
+                </HStack>
+              </FormControl>
+              <FormControl>
+                <FormLabel textAlign={"center"}>Verify New MPIN</FormLabel>
+                <HStack spacing={6} justifyContent={"center"}>
+                  <PinInput
+                    otp
+                    onChange={(value) =>
+                      MpinFormik.setFieldValue("new_mpin_confirmation", value)
+                    }
+                  >
+                    <PinInputField bg={"aqua"} />
+                    <PinInputField bg={"aqua"} />
+                    <PinInputField bg={"aqua"} />
+                    <PinInputField bg={"aqua"} />
+                  </PinInput>
+                </HStack>
+              </FormControl>
+              <Button colorScheme={"orange"} onClick={sendOtp}>
+                Done
+              </Button>
+            </VStack>
+          </Box>
+        </HStack>
+      </DashboardWrapper>
+
+
+      <Modal isOpen={isOpen} onClose={onToggle} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader textAlign={"center"}>
+            Enter OTP Sent To Your Phone
+          </ModalHeader>
+          <ModalBody>
+            <HStack justifyContent={"center"}>
+              <PinInput
+                otp
+                onComplete={(value) => MpinFormik.setFieldValue("otp", value)}
+              >
+                <PinInputField bg={"aqua"} />
+                <PinInputField bg={"aqua"} />
+                <PinInputField bg={"aqua"} />
+                <PinInputField bg={"aqua"} />
+              </PinInput>
+            </HStack>
+          </ModalBody>
+          <ModalFooter>
+            <HStack justifyContent={"flex-end"}>
+              <Button colorScheme="twitter" onClick={handleMpinReset}>Submit</Button>
+            </HStack>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ResetMpin;
--- pages/dashboard/profile/reset-password.jsx
+++ pages/dashboard/profile/reset-password.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Box,
   Stack,
@@ -8,89 +8,161 @@ import {
   FormLabel,
   Input,
   Button,
-  useToast
-} from '@chakra-ui/react'
+  useToast,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { useFormik } from "formik";
-import DashboardWrapper from '../../../hocs/DashboardLayout';
-import BackendAxios from '../../../lib/axios';
+import DashboardWrapper from "../../../hocs/DashboardLayout";
+import BackendAxios from "../../../lib/axios";
+import { Modal } from "@chakra-ui/react";
+import { ModalOverlay } from "@chakra-ui/react";
+import { ModalContent } from "@chakra-ui/react";
+import { ModalHeader } from "@chakra-ui/react";
+import { ModalBody } from "@chakra-ui/react";
+import { HStack } from "@chakra-ui/react";
+import { PinInput } from "@chakra-ui/react";
+import { PinInputField } from "@chakra-ui/react";
+import { ModalFooter } from "@chakra-ui/react";
 
 const ResetPassword = () => {
   const Toast = useToast({
-    position: 'top-right'
-  })
+    position: "top-right",
+  });
+  const { isOpen, onToggle } = useDisclosure();
 
   const PasswordFormik = useFormik({
     initialValues: {
       old_password: "",
       new_password: "",
       new_password_confirmation: "",
-    }
-  })
-
+      otp: "",
+    },
+  });
 
   function handlePasswordReset() {
-    BackendAxios.post('/api/user/new-password', JSON.stringify({
-      old_password: PasswordFormik.values.old_password,
-      new_password: PasswordFormik.values.new_password,
-      new_password_confirmation: PasswordFormik.values.new_password_confirmation,
-    })).then((res) => {
-      Toast({
-        status: 'success',
-        title: 'Success',
-        description: 'Your password was changed succesfully.'
+    BackendAxios.post(
+      "/api/user/new-password",
+      JSON.stringify({
+        old_password: PasswordFormik.values.old_password,
+        new_password: PasswordFormik.values.new_password,
+        new_password_confirmation:
+          PasswordFormik.values.new_password_confirmation,
+        otp: PasswordFormik.values.otp,
       })
-    }).catch((err) => {
-      Toast({
-        status: 'error',
-        title: 'Error Occured',
-        description: err.response.data.message || err.response.data || err.message,
+    )
+      .then((res) => {
+        Toast({
+          status: "success",
+          title: "Success",
+          description: "Your password was changed succesfully.",
+        });
       })
-    })
+      .catch((err) => {
+        Toast({
+          status: "error",
+          title: "Error Occured",
+          description:
+            err.response.data.message || err.response.data || err.message,
+        });
+      });
   }
 
+  function sendOtp() {
+    BackendAxios.post("/password/send-otp")
+      .then((res) => {
+        onToggle();
+        Toast({
+          status: "success",
+          description: "OTP Sent To Your Phone",
+        });
+      })
+      .catch((err) => {
+        Toast({
+          status: "error",
+          description:
+            err?.response?.data?.message || err?.response?.data || err?.message,
+        });
+      });
+  }
 
   return (
     <>
-      <DashboardWrapper titleText={'Reset Password'}>
+      <DashboardWrapper titleText={"Reset Password"}>
         <Box
-          bg={'white'}
-          boxShadow={'md'}
-          p={6} w={['full', 'md']}
-          rounded={16} mx={'auto'}
-          display={'flex'}
-          flexDirection={'column'}
-          justifyContent={'center'}
-          alignItems={'center'}
+          bg={"white"}
+          boxShadow={"md"}
+          p={6}
+          w={["full", "md"]}
+          rounded={16}
+          mx={"auto"}
+          display={"flex"}
+          flexDirection={"column"}
+          justifyContent={"center"}
+          alignItems={"center"}
         >
-          <Text fontSize={'lg'} mb={12}>Reset Your Password</Text>
+          <Text fontSize={"lg"} mb={12}>
+            Reset Your Password
+          </Text>
           <VStack spacing={8}>
             <FormControl>
-              <FormLabel textAlign={'center'}>Enter Old Passwod</FormLabel>
+              <FormLabel textAlign={"center"}>Enter Old Passwod</FormLabel>
               <Input
-                name={'old_password'}
+                name={"old_password"}
                 onChange={PasswordFormik.handleChange}
               />
             </FormControl>
-            <FormControl >
-              <FormLabel textAlign={'center'}>Enter New Password</FormLabel>
+            <FormControl>
+              <FormLabel textAlign={"center"}>Enter New Password</FormLabel>
               <Input
-                name={'new_password'} type={'password'}
+                name={"new_password"}
+                type={"password"}
                 onChange={PasswordFormik.handleChange}
               />
             </FormControl>
-            <FormControl >
-              <FormLabel textAlign={'center'}>Confirm New Password</FormLabel>
+            <FormControl>
+              <FormLabel textAlign={"center"}>Confirm New Password</FormLabel>
               <Input
-                name={'new_password_confirmation'}
+                name={"new_password_confirmation"}
                 onChange={PasswordFormik.handleChange}
               />
             </FormControl>
-            <Button colorScheme={'orange'} onClick={handlePasswordReset}>Done</Button>
+            <Button colorScheme={"orange"} onClick={sendOtp}>
+              Done
+            </Button>
           </VStack>
         </Box>
       </DashboardWrapper>
-    </>
-  )
-}
 
-export default ResetPassword
+      <Modal isOpen={isOpen} onClose={onToggle} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader textAlign={"center"}>
+            Enter OTP Sent To Your Phone
+          </ModalHeader>
+          <ModalBody>
+            <HStack justifyContent={"center"}>
+              <PinInput
+                otp
+                onComplete={(value) => PasswordFormik.setFieldValue("otp", value)}
+              >
+                <PinInputField bg={"aqua"} />
+                <PinInputField bg={"aqua"} />
+                <PinInputField bg={"aqua"} />
+                <PinInputField bg={"aqua"} />
+              </PinInput>
+            </HStack>
+          </ModalBody>
+          <ModalFooter>
+            <HStack justifyContent={"flex-end"}>
+              <Button colorScheme="twitter" onClick={handlePasswordReset}>
+                Submit
+              </Button>
+            </HStack>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ResetPassword;
