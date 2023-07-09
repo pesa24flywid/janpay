8a9fba559bf0df808bde8402ca0dffa1098b8e0a
DezyNation <dezynationindia@gmail.com>
2023-07-09 11:19:49 +0530

    server down

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,20 @@
 import React, { useEffect } from 'react'
 import Head from 'next/head'
+import { VStack } from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
 
 const Home = () => {
-  useEffect(() => {
-    window.location.assign('/dashboard')
-  }, [])
+  // useEffect(() => {
+  //   window.location.assign('/dashboard')
+  // }, [])
 
   return (
     <>
-      <Head><title>Janpay - Home</title></Head>
-      Home
+      <Head><title>Janpay - Maintenance</title></Head>
+      <VStack w={'full'} h={'100vh'}>
+        <Text fontSize={'xl'} textAlign={'center'}>We are updating our servers</Text>
+        <Text fontSize={'xl'} textAlign={'center'}>Please try again after 11:45 IST</Text>
+      </VStack>
     </>
   )
 }
